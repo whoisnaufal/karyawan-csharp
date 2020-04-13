e044ef90ec0d436cc8b98ef2e13e7d7a92239a9d
naufal <naufalhaxor78@gmail.com>
2020-04-13 14:48:14 +0700

    final commit

--- Program.cs
+++ Program.cs
@@ -13,14 +13,11 @@ namespace ResponsiPemrograman2643
 			Karyawan karyawan1 = new Karyawan(1,"190302123", "Naufal", 3000000);
 			Karyawan karyawan2 = new Karyawan(2,"190302124", "Joni", 2000000);
 
-			Console.WriteLine("No Nik/Nama\tGaji Bulanan");
-			Console.WriteLine("------------------------------");
+			header1();
 			CetakGaji(karyawan1);
 			CetakGaji(karyawan2);
 
-			Console.WriteLine("\nKenaikan Gaji 10%");
-			Console.WriteLine("No Nik/Nama\tGaji Bulanan");
-			Console.WriteLine("------------------------------");
+			header2();
 			NaikGaji(karyawan1);
 			NaikGaji(karyawan2);
 
@@ -29,7 +26,7 @@ namespace ResponsiPemrograman2643
 
 		static void CetakGaji(Karyawan kry)
 		{
-			Console.WriteLine(kry.No +". "+ kry.Nik + " " + kry.Nama + " " + kry.GajiBulanan);
+			Console.WriteLine(kry.No +". "+ kry.Nik + "\t" + kry.Nama + "\t" + kry.GajiBulanan);
 		}
 
 		static void NaikGaji(Karyawan kry)
@@ -37,7 +34,20 @@ namespace ResponsiPemrograman2643
 			double kenaikan;
 			kenaikan = kry.GajiBulanan * 0.1;
 			kry.GajiBulanan = kry.GajiBulanan + kenaikan;
-			Console.WriteLine(kry.No + ". " + kry.Nik + " " + kry.Nama + " " + kry.GajiBulanan);
+			Console.WriteLine(kry.No + ". " + kry.Nik + "\t" + kry.Nama + "\t" + kry.GajiBulanan);
+		}
+
+		static void header1()
+		{
+			Console.WriteLine("No Nik/Nama\tGaji Bulanan");
+			Console.WriteLine("------------------------------");
+		}
+
+		static void header2()
+		{
+			Console.WriteLine("\nKenaikan Gaji 10%");
+			Console.WriteLine("No Nik/Nama\tGaji Bulanan");
+			Console.WriteLine("------------------------------");
 		}
 	}
 }
